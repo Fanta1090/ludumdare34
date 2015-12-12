a39ab8428f4e38d88af57074bdcfb6d0d5a16954
Supermomo <maugard.matthieu@gmail.com>
2015-12-12 10:29:59 +0100

    Added background image

--- src/WindowGame.java
+++ src/WindowGame.java
@@ -6,40 +6,38 @@ import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class WindowGame extends BasicGame {
-	
-	GameContainer container;
-	
-	public WindowGame()
-	{
-		super("LD34");
-	}
-	
-	public static void main(String[] args) throws SlickException {
-        new AppGameContainer(new WindowGame(), 640, 480, false).start();
+
+  GameContainer container;
+
+  public WindowGame() {
+    super("LD34");
+  }
+
+  public static void main(String[] args) throws SlickException {
+    new AppGameContainer(new WindowGame(), 640, 480, false).start();
+  }
+
+  @Override
+  public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void init(GameContainer container) throws SlickException {
+    this.container = container;
+
+  }
+
+  @Override
+  public void update(GameContainer gameContainer, int delta) throws SlickException {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void keyReleased(int key, char c) {
+    if (Input.KEY_ESCAPE == key) {
+      container.exit();
     }
-
-	@Override
-	public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void init(GameContainer container) throws SlickException {
-		this.container = container;
-
-	}
-
-	@Override
-	public void update(GameContainer gameContainer, int delta) throws SlickException {
-		// TODO Auto-generated method stub
-	}
-	
-	@Override
-    public void keyReleased(int key, char c) {
-        if (Input.KEY_ESCAPE == key) {
-            container.exit();
-        }
-    }
-
+  }
 }
